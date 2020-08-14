6045b8e017c7aa2e70d50dc0b53917d85038f153
CodingGirl <spunase@gmail.com>
2020-08-13 23:04:38 -0500

    jupyter notebook

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -4,7 +4,7 @@
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM salaries
 INNER JOIN employees ON
-employees.emp_no=salaries.emp_no
+employees.emp_no=salaries.emp_no;
 
 --2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date 
